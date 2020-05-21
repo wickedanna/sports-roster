bf4a7585db26e8c7cf450cb8c5fbce9bd8af0539
Kayla Melton <kannmelton2@gmail.com>
2020-05-20 21:47:16 -0500

    log in working

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import fbConnection from '../helpers/data/connection';
 
 import Auth from '../components/Auth/Auth';
 import MyNavbar from '../components/MyNavbar/MyNabar';
@@ -6,9 +9,25 @@ import Team from '../components/Team/Team';
 
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
   state = {
     authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   render() {
