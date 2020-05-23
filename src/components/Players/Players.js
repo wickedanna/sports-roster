b89a7e5b4f5a1d62517e8902b21a88dfae839d0c
Kayla Melton <kannmelton2@gmail.com>
2020-05-23 10:55:43 -0500

    added data and created players component

---
+++ src/components/Players/Players.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Players.scss';
+
+class Players extends React.Component {
+  render() {
+    return (
+      <div className="Players">
+        <h2>Players Component!</h2>
+      </div>
+    );
+  }
+}
+
+export default Players;
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import Players from '../Players/Players';
+
 import './Team.scss';
 
 class Team extends React.Component {
   render() {
     return (
       <div className="Team">
-        <h1>Team</h1>
+        <h1>Welcome to the Holyhead Harpies Roster</h1>
+        <Players />
       </div>
     );
   }
